d67078d2257828ffdaa8869eb8c28bccabbadc1c
Parthiv <parthiv.c2002@gmail.com>
2023-10-26 22:36:24 +0530

    Navbar UI improvements

--- components/UnifiedNavbar.tsx
+++ components/UnifiedNavbar.tsx
@@ -5,24 +5,28 @@ import Logo from "../public/Swap-removebg.png";
 import styles from "../assets/styles/UnifiedNavbar.module.css";
 import { Lato } from "next/font/google";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useRouter } from "next/router";
+
 
 const UnifiedNavbar = () => {
+  const router = useRouter();
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        <Link href="./landing">
+        <Link href="/landing">
           <Image src={Logo} alt="Logo" width={90} height={50} />
         </Link>
       </div>
       <ul className={styles.links}>
         <li>
-          <Link href="./swap">Swap</Link>
+          <Link href="/swap" className={router.pathname === "/swap" ? styles.activeLink : ""}>Swap</Link>
         </li>
         <li>
-          <Link href="./tokens">Tokens</Link>
+          <Link href="/tokens" className={router.pathname === "/tokens" ? styles.activeLink : ""}>Tokens</Link>
         </li>
         <li>
-          <Link href="./pools">Pools</Link>
+          <Link href="/pools" className={router.pathname === "/pools" ? styles.activeLink : ""}>Pools</Link>
         </li>
       </ul>
       <div className={styles.button}>
@@ -36,5 +40,6 @@ const UnifiedNavbar = () => {
     </nav>
   );
 };
+
 
 export default UnifiedNavbar;
