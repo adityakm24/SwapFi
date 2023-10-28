9fba3ca34aefb3e9e60cb6db7a1c60addad61788
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2023-10-28 18:24:43 +0530

    UI updates

--- components/Liquidity.tsx
+++ components/Liquidity.tsx
@@ -1,29 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAccount, useNetwork } from "wagmi";
-import UnifiedNavbar from "./UnifiedNavbar";
-import styles from "../assets/styles/Swap.module.css";
+import styles from "../assets/styles/Liquidity.module.css";
 import useIsMounted from "./hooks/useIsMounted";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose, faGear, faSearch, faExchangeAlt } from "@fortawesome/free-solid-svg-icons"; // Import the swap icon.
+import {
+  faClose,
+  faGear,
+  faSearch,
+  faExchangeAlt,
+} from "@fortawesome/free-solid-svg-icons"; // Import the swap icon.
 
 const Swap: React.FC = () => {
   const router = useRouter();
   const mounted = useIsMounted();
   const account = useAccount();
-  
 
   const [payCoin, setPayCoin] = useState("ETH");
   const [receiveCoin, setReceiveCoin] = useState("XDC");
-  const [payValue, setPayValue] = useState(0);
-  const [receiveValue, setReceiveValue] = useState(0);
+  const [tokenA, settokenA] = useState(0);
+  const [tokenB, settokenB] = useState(0);
   const [showPayModal, setShowPayModal] = useState(false);
   const [showReceiveModal, setShowReceiveModal] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredCoins, setFilteredCoins] = useState<string[]>([]);
 
-  const [OWChainName, setOWChainName] = useState("");
-  const [OAddress, setOAddress] = useState("");
+  const [maxSlippage, setMaxSlippage] = useState<bigint>(0);
+  const [transactionDeadline, setTransactionDeadline] = useState<bigint>();
+
+  const handleMaxSlippageChange = (e) => {
+    setMaxSlippage(e.target.value);
+  };
+
+  const handleTransactionDeadlineChange = (e) => {
+    setTransactionDeadline(e.target.value);
+  };
+
+    
 
   const closeModal = () => {
     setShowPayModal(false);
@@ -35,32 +48,24 @@ const Swap: React.FC = () => {
 
   const availableCoins = ["ETH", "XDC", "BTC", "ADA", "DOGE"];
 
-  const swapCoins = () => {
-    const tempPayCoin = payCoin;
-    const tempPayValue = payValue;
-    setPayCoin(receiveCoin);
-    setReceiveCoin(tempPayCoin);
-    setPayValue(receiveValue);
-    setReceiveValue(tempPayValue);
+  const [showSettingsDropdown, setShowSettingsDropdown] = useState(false);
+
+  const openSettingsDropdown = () => {
+    setShowSettingsDropdown(true);
   };
 
-  const getIconForCoin = (coin: string) => {
-    switch (coin) {
-      case "ETH":
-        return "ethereum.png"; 
-      case "BTC":
-        return "bitcoin.png"; 
-      case "ADA":
-        return "ada.png"; 
-      case "DOGE":
-        return "dogecoin.png"; 
-      case "XDC":
-        return "xdc.png";
-      default:
-        return "";
-    }
+  const closeSettingsDropdown = () => {
+    setShowSettingsDropdown(false);
   };
-  
+
+  const swapCoins = () => {
+    const tempPayCoin = payCoin;
+    const temptokenA = tokenA;
+    setPayCoin(receiveCoin);
+    setReceiveCoin(tempPayCoin);
+    settokenA(tokenB);
+    settokenB(temptokenA);
+  };
 
   useEffect(() => {
     setFilteredCoins(
@@ -74,60 +79,130 @@ const Swap: React.FC = () => {
     <div className={styles.swapcontainer}>
       <div className={styles.swappanel}>
         <div className={styles.swapheader}>
-          <button>Swap</button>
+          <button className={styles.heading}>Add liquidity</button>
           <button className={styles.iconbtn}>
-            <FontAwesomeIcon icon={faGear} />
+            <p>{maxSlippage}% Slipage</p>
+            <FontAwesomeIcon icon={faGear} onClick={openSettingsDropdown} />
           </button>
         </div>
+        {/* Settings Dropdown */}
+        {showSettingsDropdown && (
+          <div className={styles.modalBackdrop}>
+            <div className={styles.settingsDropdown}>
+              <button
+                className={styles.closebutton}
+                onClick={closeSettingsDropdown}
+              >
+                <FontAwesomeIcon icon={faClose} />
+              </button>
+              <br></br>
+              <div className={styles.container}>
+                <div className={styles.section}>
+                  <div className={styles.labelContainer}>
+                    <div className={styles.leftLabel}>
+                      <p>Max Slippage</p>
+                    </div>
+                    <div className={styles.rightLabel}>
+                      <p>{maxSlippage}%</p>
+                    </div>
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <input
+                      type="number"
+                      value={maxSlippage}
+                      onChange={handleMaxSlippageChange}
+                      className={styles.inputField}
+                      placeholder="0"
+                    />
+                    <div className={styles.inputText}>%</div>
+                  </div>
+                </div>
+                <div className={styles.section}>
+                  <div className={styles.labelContainer}>
+                    <div className={styles.leftLabel}>
+                      <p>Transaction Deadline</p>
+                    </div>
+                    <div className={styles.rightLabel}>
+                      <p>{transactionDeadline}m</p>
+                    </div>
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <input
+                      type="number"
+                      value={transactionDeadline}
+                      onChange={handleTransactionDeadlineChange}
+                      className={styles.inputField}
+                      placeholder="0"
+                    />
+                    <div className={styles.inputText}>Minutes</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         <div className={styles.swapbody}>
           <div className={styles.boxs}>
             <div>
               <div className={styles.swapsection}>
-                <label>You pay</label>
                 <div className={styles.payinput}>
                   <input
                     type="number"
                     placeholder="0"
-                    value={payValue}
-                    onChange={(e) => setPayValue(parseFloat(e.target.value))}
+                    value={tokenA}
+                    onChange={(e) => settokenA(parseFloat(e.target.value))}
                   />
                   <button
                     className={styles.paycoin}
                     onClick={() => setShowPayModal(true)}
                   >
-                    <div style={{ display: "flex", alignItems: "center", marginLeft:"-10px"}}>
-    <img src={getIconForCoin(payCoin)} alt={payCoin} style={{ width: "30px", height: "30px", marginRight: "5px" }} />
-                    <span>{payCoin}</span>                    
+                    <span>{payCoin}</span>
                     <span>▼</span>
-                  </div>
                   </button>
                 </div>
               </div>
             </div>
             <div>
-              <button className={styles.ExButton} onClick={swapCoins}>
-                <FontAwesomeIcon icon={faExchangeAlt} style={iconStyle} />              
-                </button>
+              <p
+                className={styles.ExButton} // Add a style for the Swap button
+                onClick={swapCoins}
+              >
+                +
+              </p>
             </div>
             <div>
               <div className={styles.swapsection}>
-                <label>You receive</label>
                 <div className={styles.receiveinput}>
                   <input
                     type="number"
                     placeholder="0"
-                    value={receiveValue}
-                    onChange={(e) =>
-                      setReceiveValue(parseFloat(e.target.value))
-                    }
+                    value={tokenB}
+                    onChange={(e) => settokenB(parseFloat(e.target.value))}
                   />
                   <button onClick={() => setShowReceiveModal(true)}>
-                    <div style={{ display: "flex", alignItems: "center", marginLeft:"-10px"}}>
-    <img src={getIconForCoin(receiveCoin)} alt={receiveCoin} style={{ width: "30px", height: "30px", marginRight: "5px" }} />
-                    <span>{receiveCoin}</span>                    
+                    <span>{receiveCoin}</span>
                     <span>▼</span>
-                  </div>
                   </button>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className={styles.txtarea}>
+                <p>Prices and pool share</p>
+                <div className={styles.boxContainer}>
+                  <div className={styles.box}>
+                    <p className={styles.boxTextTop}>6210.39</p>
+                    <p className={styles.boxTextBottom}>1INCH per ETH</p>
+                  </div>
+                  <div className={styles.box}>
+                    <p className={styles.boxTextTop}>0.00016102</p>
+                    <p className={styles.boxTextBottom}>ETH per 1INCH</p>
+                  </div>
+                  <div className={styles.box}>
+                    <p className={styles.boxTextTop}>2.68%</p>
+                    <p className={styles.boxTextBottom}>Share of pool</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -163,7 +238,6 @@ const Swap: React.FC = () => {
                     closeModal();
                   }}
                 >
-                  <img src={getIconForCoin(coin)} alt={coin} style={{ width: "20px", height: "20px" , marginRight:"10px"}} />
                   {coin}
                 </button>
               ))}
@@ -193,8 +267,6 @@ const Swap: React.FC = () => {
                     closeModal();
                   }}
                 >
-                  <img src={getIconForCoin(coin)} alt={coin} style={{ width: "20px", height: "20px" , marginRight:"10px"}} />
-
                   {coin}
                 </button>
               ))}
